b6f79f08a8f5c18ffdc034c449c44893cd246dad
unknown <schmittluigi@gmail.com>
2024-02-29 18:54:09 -0300

    main

---
+++ project003.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Learning how to use variables: 
+int main()
+{
+    int myage = 19;
+    int dadage = 49;
+    int momage = 48;
+
+    printf("My age = %i\nMy dad age = %i\nMy mom age = %i\n", myage, dadage, momage);
+
+    return 0; 
+}
