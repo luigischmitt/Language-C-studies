f484ef24c8476167e2ec787f754f6bf70f5648d0
unknown <schmittluigi@gmail.com>
2024-03-02 01:50:58 -0300

    main

---
+++ project006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Learning about float and char variables
+int main()
+{
+    int integer = 10;
+    float decimal = 6.5;
+    char character = 'a';
+
+    printf ("%i\n", integer);
+    printf ("%f\n", decimal);
+    printf ("%c\n", character);
+
+    return 0;
+}
