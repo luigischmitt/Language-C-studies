798b590bf99b1289942a11df891cd4e8a89cbd7c
unknown <schmittluigi@gmail.com>
2024-03-05 15:20:20 -0300

    main

---
+++ project008.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+// Learning about for and loops
+int main()
+{
+    int counter;
+
+    for (counter = 1; counter <= 10; counter = counter + 1) {
+        printf("Luigi\n");
+        printf("and Mario\n");
+    }
+
+    return 0;
+}
