5cd60267c5a2eeb54ea7ad719233a8fab0ed76d6
unknown <schmittluigi@gmail.com>
2024-03-05 23:38:33 -0300

    main

--- project007.c
+++ project007.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-// Learning more about float operations
+// Learning more abou float operations
 int main()
 {
     int num1 = 5;
-    float num2 = 2;             
+    float num2 = 2;
     float result = num1 / num2;
 
     printf ("%f\n", result);
