da700f2de0635f24ebee09bdbeb1cd01d15d8db2
unknown <schmittluigi@gmail.com>
2024-03-05 23:38:02 -0300

    main

---
+++ project009.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Studying how to use while
+int main()
+{
+    int counter = 1;
+    while (counter <= 5) {
+        printf("%i\n", counter);
+        ++counter;
+    }
+    return 0;
+}
