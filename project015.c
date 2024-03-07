68034bd9e9f06ae8bef6bccd654e9d3f0859996d
unknown <schmittluigi@gmail.com>
2024-03-07 16:41:28 -0300

    main

---
+++ project015.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// Practicing with exercises
+int main() 
+{
+    int num1, num2;
+
+    printf("Type 2 integer numbers: ");
+    scanf("%i %i", &num1, &num2);
+
+    if (num2 == 0)
+        printf("A number cannot be divided by 0.\n");
+    else if (num1 % num2 == 0)
+        printf("%i can be divided by %i.\n", num1, num2);
+    else
+        printf("%i can't be divided by %i.\n", num1, num2);
+
+    return 0;
+}
